4ea8296b0d05ba177acefca3589796200465958b
vicziani <viczian.istvan@gmail.com>
2024-10-29 09:00:14 +0100

    Tesztek véglegesítve.

--- demo/src/test/java/training/demo/DemoApplicationIT.java
+++ demo/src/test/java/training/demo/DemoApplicationIT.java
@@ -1,13 +1,27 @@
 package training.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class DemoApplicationIT {
 
+	@Autowired
+	EmployeeController employeeController;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void addEmployee() {
+		employeeController.addEmployee(new EmployeeResource(null, "Jane Doe"));
+		assertThat(employeeController.findEmployees())
+				.extracting(EmployeeResource::name)
+				.contains("Jane Doe");
 	}
 
 }
