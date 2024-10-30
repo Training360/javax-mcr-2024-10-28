159303d0d1cf3321b2492e6d6bd229f7212f36c4
vicziani <viczian.istvan@gmail.com>
2024-10-30 12:15:47 +0100

    JPA működik!

--- demo-jsf-db/src/main/java/training/demo/EmployeeService.java
+++ demo-jsf-db/src/main/java/training/demo/EmployeeService.java
@@ -1,6 +1,7 @@
 package training.demo;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,35 +12,40 @@ import java.util.concurrent.atomic.AtomicLong;
 @Service
 public class EmployeeService {
 
-    private Map<Long, EmployeeResource> employees = new ConcurrentHashMap<>(
-            Map.of(1L, new EmployeeResource(1L, "John Doe"),
-                    2L, new EmployeeResource(2L, "Jack Doe")
-                    )
-    );
+    private EmployeeRepository employeeRepository;
 
-    private AtomicLong counter = new AtomicLong(2);
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
 
     public List<EmployeeResource> findEmployees() {
-        return new ArrayList<>(employees.values());
+        return employeeRepository.findAll()
+                .stream().map(entity -> new EmployeeResource(entity.getId(), entity.getName()))
+                .toList();
     }
 
     public EmployeeResource findEmployeeById(long id) {
-        return employees.get(id);
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Employee not found with id: %d".formatted(id)));
+        return new EmployeeResource(employee.getId(), employee.getName());
     }
 
     public EmployeeResource addEmployee(EmployeeResource employee) {
-        long id = counter.incrementAndGet();
-        EmployeeResource employeeResource = new EmployeeResource(id, employee.getName());
-        employees.put(id, employeeResource);
-        return employeeResource;
+        Employee original = new Employee(employee.getId(), employee.getName());
+        Employee entity = employeeRepository.save(original);
+
+        return new EmployeeResource(entity.getId(), entity.getName());
     }
 
+    @Transactional
     public EmployeeResource updateEmployee(EmployeeResource employee) {
-        employees.put(employee.getId(), employee);
+        Employee original = employeeRepository.findById(employee.getId())
+                .orElseThrow(() -> new IllegalArgumentException("Employee not found with id: %d".formatted(employee.getId())));
+        original.setName(employee.getName());
         return employee;
     }
 
     public void deleteEmployee(long id) {
-        employees.remove(id);
+        employeeRepository.deleteById(id);
     }
 }
