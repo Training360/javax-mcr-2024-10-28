dc557bb987d891ea4c05c22644353f510240b844
vicziani <viczian.istvan@gmail.com>
2024-10-30 14:58:48 +0100

    Tesztadat előkészítés lehetőségek

--- demo-jsf-db/src/test/java/training/demo/EmployeeEndToEndIT.java
+++ demo-jsf-db/src/test/java/training/demo/EmployeeEndToEndIT.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 
@@ -15,11 +16,17 @@ import static org.hamcrest.Matchers.equalTo;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-
-public class EmployeeEndToEndIT {
+@Sql(statements = {"delete from employee"})
+class EmployeeEndToEndIT {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @Autowired
+    EmployeeService employeeService;
 
     @BeforeEach
     void init() {
@@ -32,12 +39,16 @@ public class EmployeeEndToEndIT {
 
 
     @Test
-    void findById() {
+    void findAll() {
+//        Employee employee = employeeRepository.save(new Employee(null, "John Doe"));
+        EmployeeResource resource = employeeService.addEmployee(new EmployeeResource(null, "John Doe"));
+        long id = resource.getId();
+
         when()
-                .get("/api/employees/{id}", "1")
+                .get("/api/employees/{id}", Long.toString(id))
                 .then()
                 .statusCode(200)
-                .body("id", equalTo(1),
+                 .body("id", equalTo((int) id),
                         "name", equalTo("John Doe"));
 
     }
--- demo-jsf-db/src/test/java/training/demo/EmployeesSeleniumEndToEndTest.java
+++ demo-jsf-db/src/test/java/training/demo/EmployeesSeleniumEndToEndTest.java
@@ -1,13 +1,19 @@
 package training.demo;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,6 +23,15 @@ class EmployeesSeleniumEndToEndTest {
     @LocalServerPort
     private int port;
 
+    @Autowired
+    DataSource dataSource;
+
+    @BeforeEach
+    void employEmployeeTable() {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        jdbcTemplate.execute("delete from employee");
+    }
+
     @Test
     void addEmployee() {
         WebDriverManager.chromedriver().setup();
@@ -24,7 +39,7 @@ class EmployeesSeleniumEndToEndTest {
         driver.get("http://localhost:" + port + "/index.xhtml");
         WebElement input = driver.findElement(By.id("create-form:name-input"));
         input.clear();
-        input.sendKeys("John Doe");
+        input.sendKeys("John Doe " + UUID.randomUUID());
         driver.findElement(By.id("create-form:create-button")).click();
 
         String value = driver.findElement(By.cssSelector("#messages-ul > li")).getText();
